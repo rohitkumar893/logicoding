18a23c6b69069e76c1ec6da5ac5e2504b9b72853
Rohit Kumar <rkrohitkumar893@gmail.com>
2025-07-07 00:35:04 +0530

    add: section - react component

--- src/components/ReactTutorials.tsx
+++ src/components/ReactTutorials.tsx
@@ -39,11 +39,16 @@ const ReactTutorials: React.FC<Props> = ({ selectedKey }) => {
                 Once you have done this, you should see that your React files has been created.
       </p>;
     case 'react-setup':
-      return <p><b>React Setup:</b>
+      return <p className='text-xl'><b>React Setup:</b>
       
       </p>;
     case 'react-components':
-      return <p><b>React Components:</b>
+      return <p className='text-xl'><b>React Components:</b><br /><br />
+      Components are independent and reusable bits of code. They serve the same purpose as JavaScript functions, but work in isolation and return HTML.
+      Components come in two types, Class components and Function components.<br /><br />
+      Class Component
+      A class component must include the extends React.Component statement. This statement creates an inheritance to React.Component, and gives your component access to React.Component's functions.
+      The component also requires a render() method, this method returns HTML.
       
       </p>;
     default:
